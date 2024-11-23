0b0015784393b3bdb0f7c2a8a938280b4a849583
Nivedhika <ashalanivedhika0812@gmail.com>
2024-11-23 10:13:55 +0530

    Array.java with Nivedhika

---
+++ Array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 1, 2};
+        Arrays.sort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
